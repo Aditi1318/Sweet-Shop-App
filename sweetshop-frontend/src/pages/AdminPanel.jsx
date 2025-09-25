f687eebd0cc1c77a3c24dd6409b541c01869608b
Aditi1318 <aditikushwaha.1318@gmail.com>
2025-09-26 00:18:05 +0530

    fix error

--- sweetshop-frontend/src/pages/AdminPanel.jsx
+++ sweetshop-frontend/src/pages/AdminPanel.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from "react";
 import {Button} from "@/components/ui/Button";
 import {Input} from "@/components/ui/Input";
 import {Label} from "@/components/ui/Label";
-import {Textarea} from "@/components/ui/Textarea";
+import {Textarea} from "@/components/ui/TextArea";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/Card";
 import {
     Dialog,
